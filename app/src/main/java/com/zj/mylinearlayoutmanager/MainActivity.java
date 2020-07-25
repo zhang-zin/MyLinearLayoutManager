5ca7e02e256c39360fb2e072918319bc043a1691
zhangjin <798101251@qq.com>
2020-07-25 14:35:09 +0800

    Initial commit

---
+++ app/src/main/java/com/zj/mylinearlayoutmanager/MainActivity.java
@@ -0,0 +1,45 @@
+package com.zj.mylinearlayoutmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.zj.mylinearlayoutmanager.adapter.UniversalAdapter;
+import com.zj.mylinearlayoutmanager.adapter.ViewHolder;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private List<SlideCardBean> mData;
+    private UniversalAdapter<SlideCardBean> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.rv);
+        rv.setLayoutManager(new SlideCardLayoutManager());
+        mData = SlideCardBean.initDatas();
+
+        adapter = new UniversalAdapter<SlideCardBean>(this, mData, R.layout.rv_item_layout) {
+            @Override
+            public void convert(ViewHolder viewHolder, SlideCardBean slideCardBean) {
+                viewHolder.setText(R.id.tv, slideCardBean.getName());
+
+                Glide.with(MainActivity.this)
+                        .load(slideCardBean.getUrl())
+                        .into((ImageView) viewHolder.getView(R.id.iv));
+            }
+        };
+        rv.setAdapter(adapter);
+        CardConfig.initConfig(this);
+    }
+}
